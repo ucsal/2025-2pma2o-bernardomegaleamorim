8a92244f931c3417d92a7b07a30d1fd3708298df
Bernardo Megale <bernardomegale.amorim@ucsal.edu.br>
2025-08-29 15:25:28 -0300

    feat: aplica OCP usando DiscountPolicy, remove switch

--- src/test/java/br/com/mariojp/solid/ocp/DiscountCalculatorTest.java
+++ src/test/java/br/com/mariojp/solid/ocp/DiscountCalculatorTest.java
@@ -1,13 +1,32 @@
 package br.com.mariojp.solid.ocp;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DiscountCalculatorTest {
+
     @Test
-    void partner_gets_12_percent() {
-        var calc = new DiscountCalculator();
-        assertEquals(88.0, calc.apply(100.0, CustomerType.PARTNER), 0.0001,
-            "PARTNER deveria ter 12% de desconto");
+    public void testDiscounts() {
+        DiscountCalculator calculator = new DiscountCalculator(
+            Map.of(
+                CustomerType.REGULAR, new RegularPolicy(),
+                CustomerType.PREMIUM, new PremiumPolicy(),
+                CustomerType.PARTNER, new PartnerPolicy()
+            )
+        );
+
+        assertEquals(95.0, calculator.apply(100.0, CustomerType.REGULAR));
+        assertEquals(90.0, calculator.apply(100.0, CustomerType.PREMIUM));
+        assertEquals(88.0, calculator.apply(100.0, CustomerType.PARTNER));
+    }
+
+    @Test
+    public void testNoDiscountPolicy() {
+        DiscountCalculator calculator = new DiscountCalculator(
+            Map.of() 
+        );
+
+        assertEquals(100.0, calculator.apply(100.0, CustomerType.REGULAR));
     }
 }
